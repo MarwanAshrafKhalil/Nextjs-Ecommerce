5de2626ace817371362457f26efa7be606df4997
MarwanAshrafKhalil <marwan.a.k.1997@gmail.com>
2023-08-15 14:48:52 +0300

    .

--- components/Layout.js
+++ components/Layout.js
@@ -1,28 +1,32 @@
 import Nav from "@/components/Nav";
-import { useSession, signIn, signOut } from "next-auth/react"
+import { useSession, signIn, signOut } from "next-auth/react";
 
-export default function Layout({children}) {
+export default function Layout({ children }) {
   const { data: session } = useSession();
   // console.log("session",session)
-  if(!session) {
+  // log
+  if (!session) {
     return (
       <div className={"bg-blue-900 w-screen h-screen flex items-center"}>
         <div className={"text-center w-full"}>
-          <button onClick={ () => signIn('google')} className={"bg-white p-2 px-4 rounded-lg"}>Login with Google</button>
+          <button
+            onClick={() => signIn("google")}
+            className={"bg-white p-2 px-4 rounded-lg"}
+          >
+            Login with Google
+          </button>
+        </div>
       </div>
-    </div>
     );
   }
-  
-  return (
-   <div className="bg-blue-900 min-h-screen flex">
-    
-    <Nav/>
 
-    <div className="bg-white flex-grow mt-2 mr-2 rounded-lg p-4">
-      {children}
+  return (
+    <div className="bg-blue-900 min-h-screen flex">
+      <Nav />
+
+      <div className="bg-white flex-grow mt-2 mr-2 rounded-lg p-4">
+        {children}
+      </div>
     </div>
-  
-  </div>
   );
 }
